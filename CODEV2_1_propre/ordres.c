ccc6018b81f36c3527b20a5c973714c91dddfe35
ALAN9876 <83357356+ALAN9876@users.noreply.github.com>
2021-04-30 13:06:36 +0200

    Code binome

---
+++ CODEV2_1_propre/ordres.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
+
+#include <windows.h>
+
+#include "definition.h"
+#include "Initialisation.h"
+#include "ordres.h"
+
+
+void saisir_ordres(S_ordres tab_ordres[]){
+
+
+
+
+
+
+
+
+
+}
