df449e2d4509b9ab0f748bde7eba4ce5f350808a
ALAN9876 <83357356+ALAN9876@users.noreply.github.com>
2021-05-02 18:47:49 +0200

    fusion tab

---
+++ CODEV2_1_propre/Initialisation.h
@@ -0,0 +1,8 @@
+#ifndef INITIALISATION_H_INCLUDED
+#define INITIALISATION_H_INCLUDED
+
+
+void initialisation_joueurs(S_joueur tabinfos[]);
+void initialisation_pieces(S_unitjoueur tabpions[]);
+
+#endif // INITIALISATION_H_INCLUDED
